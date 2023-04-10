40e953b07ccd3242af620b98c8da66df4e9bc95a
oliveiratullio <oliveiratulliof@gmail.com>
2023-04-10 15:59:33 -0300

    post "/tweets"

--- src/app.js
+++ src/app.js
@@ -15,6 +15,18 @@ app.post("/sign-up", (req, res) => {
     res.status(201).send("OK")
 })
 
+app.post("/tweets", (req, res) =>{
+    const { username, tweet } = req.body
+    const { user } = req.headers
+    const isHAuthorized = users.find((u) => u.username === user)
+        if (!isHAuthorized) {
+            return res.sendStatus(401)
+        }else {
+            const newTweet = { username: user, tweet }
+            tweetList.push(newTweet)
+            res.status(201).send("OK")
+        }
+})
 
 
 app.listen(5000, () => console.log('Servidor rodando na porta 5000'))
