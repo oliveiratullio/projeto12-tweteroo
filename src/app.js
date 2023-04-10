a8f9fb5f3a056e693e615861862558d96f242603
oliveiratullio <oliveiratulliof@gmail.com>
2023-04-10 17:42:56 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -2,64 +2,85 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const userList = [];
-const tweetList = [];
+const users = [];
+const tweets = [];
 
-app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body
-    const user = { username, avatar}
-    userList.push(user)
-    res.status(201).send("OK")
-})
+app.post('/sign-up', (req, res) => {
+  const { username, avatar } = req.body;
 
-app.post("/tweets", (req, res) => {
-    const { username, tweet } = req.body;
-    const { user } = req.headers;
-  
-    const isAuthorized = users.find((u) => u.username === (user || username));
-    if (!isAuthorized) {
-      return res.sendStatus(401);
-    }
-  
-    const newTweet = { username: user || username, tweet };
-    tweetList.push(newTweet);
-    res.status(201).send("OK");
-  });
-  
+  const user = { username, avatar };
+  users.push(user);
 
-app.get("/tweets", (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const maxAmount = 10;
-    const startIndex = (page - 1) * maxAmount;
-    const endIndex = page * maxAmount;
+  res.status(201).send('OK');
+});
 
-  
+app.post('/tweets', (req, res) => {
+  const { username, tweet } = req.body;
+
+  if (!username || !tweet) {
+    return res.status(400).send('Todos os campos são obrigatórios!');
+  }
+
+  const isAuthorized = users.find((user) => user.username === username);
+
+  if (!isAuthorized) {
+    return res.sendStatus(401);
+  }
+
+  const newTweet = { username, tweet };
+  tweets.push(newTweet);
+
+  res.status(201).send('OK');
+});
+
+app.get('/tweets', (req, res) => {
+  const { page } = parseInt(req.query);
+
+  if (page) {
     if (page < 1) {
-      return res.status(400).send("Informe uma página válida!");
+      return res.status(400).send('Informe uma página válida!');
     }
-  
-    const filteredTweets = tweets.filter((tweet) => {
-      const user = userList.find((u) => u.username === tweet.username || u.avatar === tweet.username);
-      if (user) {
-        tweetList.push({ ...user, tweet: tweet.tweet });
-        return true;
-      }
-      return false;
-    });
-  
-    const viewedTweets = {
-      page: page,
-      tweets: tweetList.slice(startIndex, endIndex),
-      totalPages: Math.ceil(filteredTweets.length / maxAmount),
-    };
-  
-    res.send(viewedTweets);
-  });
-  
-  
+
+    const limit = 10;
+    const start = (page - 1) * limit;
+    const end = page * limit;
+
+    if (tweets.length > 10) {
+      const tenTweets = tweets
+        .map((t) => {
+          const newFormat = users.find((u) => u.username === t.username || u.avatar);
+          return { ...newFormat, tweet: t.tweet };
+        })
+        .slice(start, end)
+        .reverse();
+
+      return res.send(tenTweets);
+    }
+
+    const lessThan10Tweets = tweets
+      .map((t) => {
+        const newFormat = users.find((u) => u.username === t.username || u.avatar);
+        return { ...newFormat, tweet: t.tweet };
+      })
+      .reverse();
+
+    res.send(lessThan10Tweets);
+  }
+
+  const tenLastTweets = tweets
+    .map((t) => {
+      const newFormat = users.find((u) => u.username === t.username || u.avatar);
+      return { ...newFormat, tweet: t.tweet };
+    })
+    .slice(-10)
+    .reverse();
+
+  res.send(tenLastTweets);
+});
 
 
-app.listen(5000, () => console.log('Servidor rodando na porta 5000'))
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
