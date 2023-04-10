d7a58cdebf19c00dadad5b1211bd369226079c28
oliveiratullio <oliveiratulliof@gmail.com>
2023-04-10 15:07:40 -0300

    app.listen

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 import express from "express";
 import cors from "cors";
 
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const userList = [];
+const tweetList = [];
+
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = res.body
+    const user = { username, avatar}
+    userList.push(user)
+    res.status(201).send("OK")
+})
+
+
+
+app.listen(5000, () => console.log('Servidor rodando na porta 5000'))
